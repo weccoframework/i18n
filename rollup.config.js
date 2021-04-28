3e1c694a6e4c2d91c350bb7da98a4495d7cd39b7
Alexander Metzner <alexander.metzner@gmail.com>
2021-04-28 14:35:17 +0200

    build: adopt package scop and simplified tsconfig

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,23 @@
 import typescript from "rollup-plugin-typescript2"
 import commonjs from "rollup-plugin-commonjs"
 
+import { compilerOptions } from "./tsconfig.json"
+// tsconfig.json sets the module to "CommonJS" which is required for ts-node/register
+// to work. However, bundling UMD and ES5 requires the option to be set to "ESNext".
+compilerOptions.module = "ESNext"
+
 export default {
     input: "./index.ts",
 
     output: [
         {
-            file: `dist/wecco-i18n.umd.js`,
-            name: "wecco",
+            file: `dist/weccoframework-i18n.umd.js`,
+            name: "@weccoframework/i18n",
             format: "umd",
             sourcemap: true
         },
         {
-            file: `dist/wecco-i18n.es5.js`,
+            file: `dist/weccoframework-i18n.es5.js`,
             format: "es",
             sourcemap: true
         },
@@ -21,9 +26,11 @@ export default {
     plugins: [
         typescript({
             tsconfigOverride: {
-                compilerOptions: {
-                    module: "ESNext",
-                },
+                compilerOptions: compilerOptions,
+                include: [
+                    "src/**/*.ts",
+                    "index.ts",
+                ],
             },
         }),
         commonjs(),
