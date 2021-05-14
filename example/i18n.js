db6a296966f9b776f826430dfb3869d5e1d5e314
Alexander Metzner <alexander.metzner@gmail.com>
2021-05-14 09:32:06 +0200

    doc: update example

--- example/i18n.js
+++ example/i18n.js
@@ -2,7 +2,7 @@ import * as i18n from "../weccoframework-i18n.es5.js"
 
 let messageResolver
 
-export const loadingPromise = i18n.MessageResolver.create(new i18n.JsonBundleLoader(i18n.fetchJsonSource("./messages")))
+export const loadingPromise = i18n.MessageResolver.create(new i18n.JsonBundleLoader(i18n.fetchJsonByLocale("./messages")))
     .then(mr => messageResolver = i18n.registerStandardFormatters(mr))
 
 export const m = (key, ...args) => messageResolver.m(key, ...args)
