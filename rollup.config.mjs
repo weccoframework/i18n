47768b10a90d4520879c6a13fd7273220884093f
Alexander Metzner <alexander.metzner@gmail.com>
2024-04-06 14:46:17 +0200

    chore: upgrade dependencies + rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,73 @@
+import typescript from "@rollup/plugin-typescript"
+import commonjs from "@rollup/plugin-commonjs"
+import terser from "@rollup/plugin-terser"
+
+import { readFileSync } from "fs"
+const { version } = JSON.parse(readFileSync("./package.json"))
+
+
+export default [
+    {
+        input: "./index.ts",
+
+        output: [
+            {
+                file: `dist/index.mjs`,
+                name: "@weccoframework/core",
+                format: "es",
+                sourcemap: true
+            },
+
+            {
+                file: `dist/index.js`,
+                format: "cjs",
+                sourcemap: true
+            },
+        ],
+
+        plugins: [
+            typescript({
+                compilerOptions: {
+                    module: "ESNext",
+                },
+            }),
+            commonjs(),
+        ]
+    },
+
+    {
+        input: "./index.ts",
+
+        output: [
+            {
+                file: "dist/weccoframework-i18n.min.js",
+                format: "umd",
+                name: "i18n",
+                sourcemap: true,
+            },
+
+            {
+                file: "dist/weccoframework-i18n.min.mjs",
+                name: "@weccoframework/i18n",
+                format: "es",
+                sourcemap: true,
+            },
+        ],
+
+        plugins: [
+            typescript({
+                compilerOptions: {
+                    module: "ESNext",
+                },
+            }),
+            commonjs(),
+            terser({
+                format: {
+                    preamble: `/* @weccoframework/i18n v${version}; Copyright 2019 - 2023 the wecco authors. Published under the terms of the Apache License V2. */`,
+                },
+            }),
+
+        ]
+    },
+
+]
